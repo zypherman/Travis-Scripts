80848dae6cce1e5b48c1cf1866264e55f0296a45
John Anderson <jra.hockey4@gmail.com>
2016-09-07 23:28:43 -0500

    Adding comment to trigger commit

--- branchSpecificActions.py
+++ branchSpecificActions.py
@@ -1,8 +1,15 @@
-# Main method
-def main(String branchName):
-    print branchName
+import sys
 
-    sys.exit(0)
+# Main method
+def main(argv):
+    int exitStatus = 0
+    if len(argv) == 1:
+        print "Not enough arguments"
+        exitStatus = 1
+    else:
+        print("Branch Name: " + argv[1])
+
+    sys.exit(exitStatus)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
